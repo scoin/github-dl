f5dc19980f525d53cb56e7c5428f5c7bdfe8df67
scoin <greg.piccolo@gmail.com>
2016-09-20 19:47:42 -0400

    isolate types

--- src/githubdl/types.go
+++ src/githubdl/types.go
@@ -1,6 +1,13 @@
 package githubdl
 
-import "fmt"
+import (
+    "net/http"
+    "io/ioutil"
+    "fmt"
+    "encoding/json"
+    "encoding/base64"
+    "strip"
+)
 
 type Params struct {
     Language *string
@@ -13,6 +20,7 @@ type Params struct {
     User *string
     Stars *string
     In *string
+    CloneFlags *string
 }
 
 func (p Params) String() string {
@@ -68,11 +76,75 @@ type Repo struct {
     Size int `json:"size"`
     Owner Owner `json:"owner"`
     Readme string
+    Lines []string
+}
+
+func initStringSlice(width int) func(string) []string {
+    var stringSlice []string
+    i := 0
+    return func(s string) []string {
+        lineWidth := width
+        if(len(s) > width - 5){
+        lineWidth -= 10
+            for len(s) > 0 {
+                var l int
+                if(len(s) > lineWidth){
+                    l = lineWidth
+                } else {
+                    l = len(s)
+                }
+                stringSlice = append(stringSlice, s[:l])
+                s = s[l:]
+                i += 1
+            }
+        } else {
+            stringSlice = append(stringSlice, s)
+            i += 1
+        }
+        return stringSlice
+    }
+}
+
+func (repo *Repo) GenerateDisplay(lineWidth int){
+    appendString := initStringSlice(lineWidth)
+    appendString(fmt.Sprintf("%v stars, %v watchers, %v forks",repo.Stars, repo.Watchers, repo.Forks))
+    appendString("")
+    appendString(repo.Fullname)
+    appendString(repo.Description)
+    appendString("")
+    appendString(repo.HtmlUrl)
+    appendString(fmt.Sprintf("%v KB, %s", repo.Size, repo.Language))
+    appendString("")
+    stringSlice := appendString(repo.Readme)
+    repo.Lines = stringSlice
+}
+
+func (repo *Repo) DisplaySlice(lineWidth int, start int) []string {
+    if(len(repo.Lines) == 0){
+        repo.GenerateDisplay(lineWidth)
+    } 
+    return repo.Lines[start:]
+}
+
+func (repo *Repo) GetReadme() {
+    url := fmt.Sprintf("https://api.github.com/repos/%s/%s/readme", repo.Owner.Name, repo.Name)
+    resp, err := http.Get(url)
+    defer resp.Body.Close()
+    if err != nil {
+        // handle error
+    } else {
+        var r Readme
+        body, _ := ioutil.ReadAll(resp.Body)
+        json.Unmarshal(body, &r)
+        data, _ := base64.StdEncoding.DecodeString(r.Content)
+
+        repo.Readme = strip.Strip(string(data))
+    }
 }
 
 type Response struct {
     Count int `json:"total_count"`
-    Items []Repo `json:"items"`
+    Items []*Repo `json:"items"`
 }
 
 type Readme struct {
