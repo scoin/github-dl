b56437c6f3ca75ea33b73977d049e7fdbdf944f2
Greg Piccolo <greg.piccolo@gmail.com>
2016-09-20 22:10:23 -0400

    remove dependencies from source tree

--- src/strip/markdown.go
+++ src/strip/markdown.go
@@ -1,35 +1,35 @@
 package strip
 
 import "regexp"
-import "fmt"
+// import "fmt"
 
 type StringReplacer []byte
 
 func(s *StringReplacer) Replace(exp string, repl string){
     regx := regexp.MustCompile(exp)
     *s = regx.ReplaceAll(*s, []byte(repl))
-    fmt.Println(*s)
+    // fmt.Println(*s)
 }
 
 func Strip(src string) string {
     output := StringReplacer(src)
-    output.Replace("\n={2,}", "\n")
-    output.Replace("~~", "")
-    output.Replace("`{3}.*\n", "")
-    output.Replace("<(.*?)>", " ")
-    output.Replace(`^[=-]{2,}\s*$`, "")
-    // output.Replace(`[^.+?](: .*?$)?`, "")
-    output.Replace(`\s{0,2}[.*?]: .*?$`, "")
-    output.Replace(`![.*?][[(].*?[])]`, "")
-    output.Replace(`[(.*?)][[(].*?[])]`, "$1")
-    output.Replace(">", "")
-    output.Replace(`^\s{1,2}[(.*?)]: (\S+)( ".*?")?\s*$`, "")
-    output.Replace(`^#{1,6}\s*([^#]*)\s*(#{1,6})?`, "$1")
-    output.Replace(`([*_]{1,3})(\S.*?\S)`, "$2")
-    output.Replace("(`{3,})(.*?)", "$2")
-    output.Replace(`^-{3,}\s*$`, "")
-    output.Replace("`(.+?)`", "$1")
-    output.Replace("\n{2,}", "  ")
+    // output.Replace("\n={2,}", "\n")
+    // output.Replace("~~", "")
+    // output.Replace("`{3}.*\n", "")
+    // output.Replace("<(.*?)>", " ")
+    // output.Replace(`^[=-]{2,}\s*$`, "")
+    // // output.Replace(`[^.+?](: .*?$)?`, "")
+    // output.Replace(`\s{0,2}[.*?]: .*?$`, "")
+    // output.Replace(`![.*?][[(].*?[])]`, "")
+    // output.Replace(`[(.*?)][[(].*?[])]`, "$1")
+    // output.Replace(">", "")
+    // output.Replace(`^\s{1,2}[(.*?)]: (\S+)( ".*?")?\s*$`, "")
+    // output.Replace(`^#{1,6}\s*([^#]*)\s*(#{1,6})?`, "$1")
+    // output.Replace(`([*_]{1,3})(\S.*?\S)`, "$2")
+    // output.Replace("(`{3,})(.*?)", "$2")
+    // output.Replace(`^-{3,}\s*$`, "")
+    // output.Replace("`(.+?)`", "$1")
+    // output.Replace("\n{2,}", "  ")
     return string(output)
 }
 
